5bf65dfe65c0708e3818e9124cd9cacf43bef5da
dnikiforov1967 <dnikiforov1967@gmail.com>
2018-07-28 12:51:40 +0200

    Append package to support database operations

---
+++ dbfunc/dbfunc.go
@@ -0,0 +1,9 @@
+package dbfunc
+
+import "fmt"
+
+func ProductReturn(id int) {
+    
+    fmt.Printf("Product return\n");
+
+}
--- stmain.go
+++ stmain.go
@@ -1,7 +1,9 @@
 package main
 
 import "fmt"
+import "./dbfunc"
 
 func main() {
 	fmt.Printf("ST test started\n");
+        dbfunc.ProductReturn(0);
 }
