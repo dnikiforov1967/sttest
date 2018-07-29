e96b5fc51afed2252a4be4f2315ec393472eba7a
dnikiforov1967 <dnikiforov1967@gmail.com>
2018-07-29 19:32:30 +0200

    Implement stub type

--- asyncservice/asyncservice.go
+++ asyncservice/asyncservice.go
@@ -1,7 +1,10 @@
 package asyncservice
 
 import "net/http"
+import "encoding/json"
 
 func AcceptPriceRequest(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusAccepted)
+    response := AsyncResponse{"price/1234"}
+    json.NewEncoder(w).Encode(response);
 }
---
+++ asyncservice/asynctypes.go
@@ -0,0 +1,5 @@
+package asyncservice
+
+type AsyncResponse struct {
+    ResourcePath string `json:"resource"`
+}
