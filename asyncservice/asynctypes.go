c46260fab5dfc41df4d637c5c5ea5b9a0ab026bd
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-07-30 11:15:10 +0200

    Append new structure and constants

--- asyncservice/asynctypes.go
+++ asyncservice/asynctypes.go
@@ -16,8 +16,22 @@ type taskCounter struct {
 	counter uint64
 }
 
+type TaskResponse struct {
+	Id uint64 `json:"id"`
+	Isin string `json:"isin"`
+    Status string `json:"status"`
+	Price float64 `json:"price"`
+	PriceDate string `json:"date"`
+}
+
+
 func (tc *taskCounter) getTaskId() uint64 {
 	return atomic.AddUint64(&tc.counter,1)
 }
 
 var TaskCounter taskCounter = taskCounter{0}
+
+const StatusCompleted string = "COMPLETED"
+const StatusInProgress string = "IN PROGRESS"
+const StatusFailed string = "FAILED"
+const StatusTimedOut string = "TIMED OUT"
