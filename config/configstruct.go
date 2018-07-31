5883ccf2056dc332184343d5e4ad2098af4ac71b
dnikiforov1967 <dnikiforov1967@gmail.com>
2018-07-31 21:29:19 +0200

    Append limit structure

--- config/configstruct.go
+++ config/configstruct.go
@@ -4,10 +4,12 @@ import (
 	"io/ioutil"
 	"log"
 	"encoding/json"
+        "../access"
 )
 
 type ConfigStruct struct {
 	Timeout int64 `json:"timeout"`
+        Limits []access.AccessLimitStruct `json:"limits"`
 }
 
 var GlobalConfig ConfigStruct = ConfigStruct{}
