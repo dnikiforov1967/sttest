3ccedf2fe78cf782108866fa073f5ecaff39e47b
dnikiforov1967 <dnikiforov1967@gmail.com>
2018-08-01 21:31:27 +0200

    Issue with Round

--- asyncservice/functions.go
+++ asyncservice/functions.go
@@ -50,7 +50,7 @@ func proceed(id uint64, isin string, underlying float64, volatility float64, sig
 	}
 	//Normal commitment
 	response.Status = StatusCompleted
-	response.Price = math.Round(underlying*volatility*100000)/100
+	response.Price = math.Ceil(underlying*volatility*1000)/100
 	response.PriceDate = time.Now().Format(time.RFC3339)
 	if signalChan != nil {
 		signalChan <- 0
