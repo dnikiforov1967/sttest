910567372a01a4dc0930cf6e51cb6c01da18bf80
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-08-02 13:51:59 +0200

    Add test

---
+++ asyncservice/functions_test.go
@@ -0,0 +1,15 @@
+package asyncservice
+
+import "testing"
+import "math"
+
+func TestRound(t * testing.T) {
+	array := []float64{1.005, 1.005001, 1.0049999}
+
+	for _, value := range array {
+		res := Round2(value)
+		if res != math.Round(value*100)/100 {
+			t.Errorf("Incorrect rounding of %f, result is %f",value,res)
+		}
+	}
+}
